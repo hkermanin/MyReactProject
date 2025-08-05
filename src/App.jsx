64618772fbf39994c2d4020ad7b6846e6c99a162
kermani <97584991+hkermanin@users.noreply.github.com>
2025-08-05 06:58:07 +0000

    section-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,27 @@
-import {useEffect, useState} from "react";
-
+import axios from "axios";
 import "./App.css";
-
-import  axios from "axios";
+import {useState} from "react";
 
 export default function App() {
 
-  const [catFact, setCatFact] = useState("");
+  const [name, setName] = useState("");
+  const [predictedAge, setPredictedAge] = useState({});
 
-  useEffect(()=>{
-    fetchData();
-  },[])
-
-  function fetchData(){
-    axios.get('https://catfact.ninja/fact').then((res)=>{
-      console.log(res.data)
-      setCatFact(res.data.fact);
+  function pAge(){
+    axios.get(`http://api.agicy.io/?name=${name}`).then((response)=>{
+      setPredictedAge(response.data);
+      console.log(response.data);
     })
   }
 
-
   return (
    <div className="app">
-     
-     <button onClick={fetchData}>Cat</button>
 
-     <h1>{catFact}</h1>
-
+     <input onChange={(e)=>setName(e.target.value)} type="text" placeholder="Enter your name"/>
+     <button onClick={pAge}>Age</button>
+     <h3>Your name is: {predictedAge?.name}</h3>
+     <h3>Predicted Age: {predictedAge?.age}</h3>
+    
      
    </div>
   );
