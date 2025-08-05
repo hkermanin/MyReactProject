d78e61e799ae6c5530c3d9491e2a3c5e564ba717
kermani <97584991+hkermanin@users.noreply.github.com>
2025-08-05 06:44:03 +0000

    section-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,32 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 import "./App.css";
-import List from "./list";
+
+import  axios from "axios";
 
 export default function App() {
 
-  const [courselist, setCourseList] = useState([]);
-  const [courseInput, setCourseInput] = useState({});
+  const [catFact, setCatFact] = useState("");
 
-  function addCourse(){
-    setCourseList([...courselist,courseInput]);
-  } 
+  useEffect(()=>{
+    fetchData();
+  },[])
 
-  function delList(i){
-    setCourseList(courselist.filter((course, index) => index !== i));
-  }
-
-  function completed(i){
-    const newList = courselist.map((course, index) => {
-      if(index === i){
-        return {...course,completed:!course.completed};
-      }else{
-        return course;
-      }
+  function fetchData(){
+    axios.get('https://catfact.ninja/fact').then((res)=>{
+      console.log(res.data)
+      setCatFact(res.data.fact);
     })
-
-    setCourseList(newList);
   }
+
 
   return (
    <div className="app">
      
-     <div className="add">
-       <input onChange={(e)=>setCourseInput({course: e.target.value, completed:false})} type="text"/>
-        <button onClick={addCourse}>Add</button>
-     </div>
+     <button onClick={fetchData}>Cat</button>
 
-     {courselist.map((course,i) =>{
-      return(
-        <List key={i} completed={completed} delList={delList} course={course} i={i} />
-      )
-     })}
+     <h1>{catFact}</h1>
 
-     
      
    </div>
   );
